2f5c4c78635f0f394359c9abde1fc6ed7a7d20e1
Ilya Alekseenkov <rmfighht@yandex.ru>
2021-08-27 22:57:59 +0300

    Выполнил задание 2 лекции 7

--- src/js/__tests__/test-app.js
+++ src/js/__tests__/test-app.js
@@ -1,59 +1,8 @@
-import orderByProps from '../app';
+import formatTelNumber from '../app';
 
-test('sorting the array according to the passed rule', () => {
-  const expected = [
-    { key: 'name', value: 'мечник' },
-    { key: 'level', value: 2 },
-    { key: 'attack', value: 80 },
-    { key: 'defence', value: 40 },
-    { key: 'health', value: 10 },
-  ];
+test('the formats is correct', () => {
+  const expected = '+79270000000';
+  const receiver = formatTelNumber('8 (927) 000-00-00');
 
-  const obj = {
-    name: 'мечник',
-    health: 10,
-    level: 2,
-    attack: 80,
-    defence: 40,
-  };
-
-  const received = orderByProps(obj, ['name', 'level']);
-
-  expect(received).toEqual(expected);
-});
-
-test('sorting an array without a passed rule', () => {
-  const expected = [
-    { key: 'attack', value: 80 },
-    { key: 'defence', value: 40 },
-    { key: 'health', value: 10 },
-    { key: 'level', value: 2 },
-    { key: 'name', value: 'мечник' },
-  ];
-
-  const obj = {
-    name: 'мечник',
-    health: 10,
-    level: 2,
-    attack: 80,
-    defence: 40,
-  };
-
-  const received = orderByProps(obj);
-
-  expect(received).toEqual(expected);
-});
-
-test('sorting an array by an incorrect rule', () => {
-  const obj = {
-    name: 'мечник',
-    health: 10,
-    level: 2,
-    attack: 80,
-    defence: 40,
-  };
-
-  expect(() => orderByProps(obj, ['prop'])).toThrowError(
-    new Error('Check the sorting parameters. One of the specified properties is missing in the object'),
-  );
+  expect(receiver).toBe(expected);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,11 @@
-export default function orderByProps(object, rulesSort) {
-  const arr = [];
-  // Отсортированный массив из свойств объекта по алфавиту:
-  const sortKeysObject = Object.keys(object).sort();
+export default function formatTelNumber(telNumber) {
+  const regExpMain = /(\s)|([()])|(-)/g;
+  const regExpEight = /^8/g;
 
-  // Функция добавляет отсортированные объекты в массив согласно задания:
-  const pushObjectsToArr = () => {
-    for (const elem of sortKeysObject) {
-      arr.push({ key: elem, value: object[elem] });
-    }
-  };
+  let telNumberCorrect = telNumber;
 
-  // ? Если правило сортировки не передано,
-  // то возвращаем массив объектов, отсортированных по алфавиту:
-  if (!rulesSort) {
-    pushObjectsToArr();
-    return arr;
-  }
+  telNumberCorrect = telNumberCorrect.replace(regExpMain, '');
+  telNumberCorrect = telNumberCorrect.replace(regExpEight, '+7');
 
-  // ? Если правило сортировки передано, но хотя бы одно из свойств отсутствует в объекте:
-  if (rulesSort.some((elem) => !Object.prototype.hasOwnProperty.call(object, elem))) {
-    throw new Error('Check the sorting parameters. One of the specified properties is missing in the object');
-  }
-
-  // ? Если правило сортировки передано корректно:
-  for (const key in object) {
-    if (rulesSort.includes(key)) {
-      // Добавляем объекты в массив согласно переданному правилу:
-      arr[rulesSort.indexOf(key)] = { key, value: object[key] };
-      // Удаляем из отсортированного массива уже добавленные свойства:
-      sortKeysObject.splice(sortKeysObject.indexOf(key), 1);
-    }
-  }
-  // Добавляем оставшиеся элементы отсортированного массива:
-  pushObjectsToArr();
-  return arr;
+  return telNumberCorrect;
 }
